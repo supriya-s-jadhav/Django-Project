424aeae9393252ba16e3318296326ea38f02eea9
Supriya Jadhav <sjadhav@SJADHAVM011048.local>
2020-02-24 10:41:17 -0600

    Hello basic web app

---
+++ django_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sjadhav/Documents/1.1AIC/4.LearningAndCertification/Django-Projects/django_venv/bin/Python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
